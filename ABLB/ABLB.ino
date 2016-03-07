b06c6384ceee399bae53e73aa3dfd2469c666c46
Keith Woeltje <comm@woeltje.org>
2016-03-06 19:26:38 -0600

    ABLB basic - it works
    Modified SparkFun MostBasic follower code. It works, but has trouble on
    tighter curves.

--- ABLB/ABLB.ino
+++ ABLB/ABLB.ino
@@ -28,14 +28,23 @@ const uint8_t SX1509_ADDRESS = 0x3E;  // SX1509 I2C address (00)
 
 SensorBar mySensorBar(SX1509_ADDRESS);
 
-const int ButtonPin = 0;
-int buttonVal = 0;
-boolean goFlag = false;
-int currentPosition = 0;
-int lastPosition = 0;
-int error = 0;
+// HOLD my code for later
+//const int ButtonPin = 0;
+//int buttonVal = 0;
+//boolean goFlag = false;
+//int currentPosition = 0;
+//int lastPosition = 0;
+//int error = 0;
 
+// SparkFun MostBasicFollower code
+// Define the states that the decision making machines uses:
+#define IDLE_STATE 0
+#define READ_LINE 1
+#define GO_FORWARD 2
+#define GO_LEFT 3
+#define GO_RIGHT 4
 
+uint8_t state = 0;
 
 // Romeo standard pins
 int Lmotor = 5;                // M1 Speed Control
@@ -69,39 +78,87 @@ void setup() {
   Serial.println();
   */
   
-}
-
-}
+} // end setup()
 
 void loop() {
 
-  buttonVal = analogRead(ButtonPin);
-  
-  if (buttonVal < 30){       // button 1
-    halt();
-    goFlag = false;
+//  HOLD this section for later use
+//  buttonVal = analogRead(ButtonPin);
+//  
+//  if (buttonVal < 30){       // button 1
+//    halt();
+//    goFlag = false;
+//  }
+//  /*
+//  else if (buttonVal < 175){ // button 2 
+//    // for initialization routine if needed	
+//  }
+//  else if (buttonVal < 360){  // button 3 
+//    // for future use
+//  }
+//  else if (buttonVal < 540){  // button 4
+//    // for future use
+//  }
+//  */
+//  else if (buttonVal < 800){  // button 5
+//    goFlag = true;
+//  }
+//  
+//  if (goFlag) {
+//    currentPosition = mySensorBar.getPosition();
+//    error = currentPosition - lastPosition;
+//    lastPosition = currentPosition;	
+//  }
+
+//SparkFun MostBasic Follower, modified for ABLB motor code
+
+  uint8_t nextState = state;
+  switch (state) {
+  case IDLE_STATE:
+    halt();       // Stops both motors
+    nextState = READ_LINE;
+    break;
+  case READ_LINE:
+    if( mySensorBar.getDensity() == 0)
+    {
+      nextState = IDLE_STATE;
+      break;
+    }
+    else if( mySensorBar.getDensity() < 7 )
+    {
+      nextState = GO_FORWARD;
+      if( mySensorBar.getPosition() < -50 )
+      {
+        nextState = GO_LEFT;
+      }
+      if( mySensorBar.getPosition() > 50 )
+      {
+        nextState = GO_RIGHT;
+      }
+    }
+    else
+    {
+      nextState = IDLE_STATE;
+    }
+    break;
+  case GO_FORWARD:
+    fwd(128,128);
+    nextState = READ_LINE;
+    break;
+  case GO_LEFT:
+    fwd(16,160); // L a little slower R a bit faster
+    nextState = READ_LINE;
+    break;
+  case GO_RIGHT:
+    fwd(160, 16);
+    nextState = READ_LINE;
+    break;
+  default:
+    halt();       // Stops both motors
+    break;
   }
-  /*
-  else if (buttonVal < 175){ // button 2 
-    // for initialization routine if needed	
-  }
-  else if (buttonVal < 360){  // button 3 
-    // for future use
-  }
-  else if (buttonVal < 540){  // button 4
-    // for future use
-  }
-  */
-  else if (buttonVal < 800){  // button 5
-    goFlag = true;
-  }
-  
-  if (goFlag) {
-    currentPosition = mySensorBar.getPosition();
-    error = currentPosition - lastPosition;
-    lastPosition = currentPosition;
-  	
-  }
+  state = nextState;
+  //delay(100);
 
 
 } // end loop()
