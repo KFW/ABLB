aea4badb7c6f6abf5228b110136bdf8f3cb37ef7
Keith Woeltje <comm@woeltje.org>
2016-03-13 11:03:15 -0500

    starting PD code
    take out SparkFun basic code and start with my own code with PID
    (really just PD) correction

--- ABLB/ABLB.ino
+++ ABLB/ABLB.ino
@@ -5,13 +5,17 @@
  * https://www.servocity.com/html/actobitty_2_wheel_robot_kit.html#.VthCuvkrI-U
  * 
  * microcontroller board DFRobot Romeo 2 (has built in motor controller):
- * http://www.dfrobot.com/wiki/index.php/Romeo_V2-All_in_one_Controller_(R3)_(SKU:DFR0225)
+ * http://www.dfrobot.com/wiki/index.php/Romeo_V2-All_in_one_Controller_(R3)_(SKU:DFR0225) 
  *
  * mount panel for board:
  * http://www.thingiverse.com/thing:1377159
  * 
  * line follower array from Sparkfun:
  * https://github.com/sparkfun/Line_Follower_Array
+ * position provided ranges from -127 (far L) to 127 (far R)
+ * 
+ * PID quick tutorial
+ * http://letsmakerobots.com/node/39972
  *  
  */
 
@@ -28,23 +32,15 @@ const uint8_t SX1509_ADDRESS = 0x3E;  // SX1509 I2C address (00)
 
 SensorBar mySensorBar(SX1509_ADDRESS);
 
-// HOLD my code for later
-//const int ButtonPin = 0;
-//int buttonVal = 0;
-//boolean goFlag = false;
-//int currentPosition = 0;
-//int lastPosition = 0;
-//int error = 0;
+const float Kp = 0.5
+const float Kd = 
 
-// SparkFun MostBasicFollower code
-// Define the states that the decision making machines uses:
-#define IDLE_STATE 0
-#define READ_LINE 1
-#define GO_FORWARD 2
-#define GO_LEFT 3
-#define GO_RIGHT 4
-
-uint8_t state = 0;
+const int ButtonPin = 0;
+int buttonVal = 0;
+boolean goFlag = false;
+int currentPosition = 0;
+int lastPosition = 0;
+int error = 0;
 
 // Romeo standard pins
 int Lmotor = 5;                // M1 Speed Control
@@ -82,16 +78,15 @@ void setup() {
 
 void loop() {
 
-//  HOLD this section for later use
-//  buttonVal = analogRead(ButtonPin);
-//  
-//  if (buttonVal < 30){       // button 1
-//    halt();
-//    goFlag = false;
-//  }
-//  /*
+  buttonVal = analogRead(ButtonPin);
+  
+  if (buttonVal < 30){       // button 1
+    halt();
+    goFlag = false;
+  }
+
 //  else if (buttonVal < 175){ // button 2 
-//    // for initialization routine if needed	
+//    // for future use	
 //  }
 //  else if (buttonVal < 360){  // button 3 
 //    // for future use
@@ -99,66 +94,19 @@ void loop() {
 //  else if (buttonVal < 540){  // button 4
 //    // for future use
 //  }
-//  */
-//  else if (buttonVal < 800){  // button 5
-//    goFlag = true;
-//  }
-//  
-//  if (goFlag) {
-//    currentPosition = mySensorBar.getPosition();
-//    error = currentPosition - lastPosition;
-//    lastPosition = currentPosition;	
-//  }
 
-//SparkFun MostBasic Follower, modified for ABLB motor code
-
-  uint8_t nextState = state;
-  switch (state) {
-  case IDLE_STATE:
-    halt();       // Stops both motors
-    nextState = READ_LINE;
-    break;
-  case READ_LINE:
-    if( mySensorBar.getDensity() == 0)
-    {
-      nextState = IDLE_STATE;
-      break;
-    }
-    else if( mySensorBar.getDensity() < 7 )
-    {
-      nextState = GO_FORWARD;
-      if( mySensorBar.getPosition() < -50 )
-      {
-        nextState = GO_LEFT;
-      }
-      if( mySensorBar.getPosition() > 50 )
-      {
-        nextState = GO_RIGHT;
-      }
-    }
-    else
-    {
-      nextState = IDLE_STATE;
-    }
-    break;
-  case GO_FORWARD:
-    fwd(128,128);
-    nextState = READ_LINE;
-    break;
-  case GO_LEFT:
-    fwd(16,160); // L a little slower R a bit faster
-    nextState = READ_LINE;
-    break;
-  case GO_RIGHT:
-    fwd(160, 16);
-    nextState = READ_LINE;
-    break;
-  default:
-    halt();       // Stops both motors
-    break;
+  else if (buttonVal < 800){  // button 5
+    goFlag = true;
+    delay(3000); //  3 sec delay to back off
+    // include visual indicator later
   }
-  state = nextState;
-  //delay(100);
+  
+  if (goFlag) {
+    currentPosition = mySensorBar.getPosition();
+    error = currentPosition - lastPosition;
+    lastPosition = currentPosition;	
+  }
+
 
 
 } // end loop()
@@ -175,24 +123,26 @@ void fwd(byte a,byte b)       // Move forward
   analogWrite (Rmotor,b);    
   digitalWrite(Rdir,LOW);
 }  
-void rev(byte a,byte b)       // Reverse
-{
-  analogWrite (Lmotor,a);
-  digitalWrite(Ldir,HIGH);   
-  analogWrite (Rmotor,b);    
-  digitalWrite(Rdir,HIGH);
-}  
-void spinR(byte a, byte b)
-{
-  analogWrite (Lmotor,a);
-  digitalWrite(Ldir,LOW);    // L fwd, R rev to spin R (clockwise)
-  analogWrite (Rmotor,b);    
-  digitalWrite(Rdir,HIGH);
-}  
-void spinL(byte a, byte b)
-{
-  analogWrite (Lmotor,a);
-  digitalWrite(Ldir,HIGH);    // R fwd, L rev to spin L (counterclockwise)
-  analogWrite (Rmotor,b);    
-  digitalWrite(Rdir,LOW);
-}  
+
+// don't need these functions for line follower
+//void rev(byte a,byte b)       // Reverse
+//{
+//  analogWrite (Lmotor,a);
+//  digitalWrite(Ldir,HIGH);   
+//  analogWrite (Rmotor,b);    
+//  digitalWrite(Rdir,HIGH);
+//}  
+//void spinR(byte a, byte b)
+//{
+//  analogWrite (Lmotor,a);
+//  digitalWrite(Ldir,LOW);    // L fwd, R rev to spin R (clockwise)
+//  analogWrite (Rmotor,b);    
+//  digitalWrite(Rdir,HIGH);
+//}  
+//void spinL(byte a, byte b)
+//{
+//  analogWrite (Lmotor,a);
+//  digitalWrite(Ldir,HIGH);    // R fwd, L rev to spin L (counterclockwise)
+//  analogWrite (Rmotor,b);    
+//  digitalWrite(Rdir,LOW);
+//}  
