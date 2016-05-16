a58edae28b6518828510d95da7943e2d189ff422
Keith Woeltje <comm@woeltje.org>
2016-05-15 20:08:27 -0500

    significant rewrite ABLB
    move variables into “loop” (make static)

--- ABLB/ABLB.ino
+++ ABLB/ABLB.ino
@@ -1,27 +1,27 @@
 /* ABLB
- * ActoBitty Line Bot 
- * 
- * base robot Actobotics ActoBitty:
- * https://www.servocity.com/html/actobitty_2_wheel_robot_kit.html#.VthCuvkrI-U
- * 
- * added faster motors 270 rpm
- * https://www.servocity.com/html/270_rpm_micro_gearmotorblocks.html#.VzjyE5MrIUE
- * 
- * microcontroller board DFRobot Romeo 2 (has built in motor controller):
- * http://www.dfrobot.com/wiki/index.php/Romeo_V2-All_in_one_Controller_(R3)_(SKU:DFR0225) 
- *
- * mount panel for board:
- * http://www.thingiverse.com/thing:1377159
- * 
- * line follower array from Sparkfun:
- * https://github.com/sparkfun/Line_Follower_Array
- * https://learn.sparkfun.com/tutorials/sparkfun-line-follower-array-hookup-guide
- * position provided ranges from -127 (far L) to 127 (far R)
- * 
- * PID quick tutorial
- * http://letsmakerobots.com/node/39972
- *  
- */
+   ActoBitty Line Bot
+
+   base robot Actobotics ActoBitty:
+   https://www.servocity.com/html/actobitty_2_wheel_robot_kit.html#.VthCuvkrI-U
+
+   added faster motors 270 rpm
+   https://www.servocity.com/html/270_rpm_micro_gearmotorblocks.html#.VzjyE5MrIUE
+
+   microcontroller board DFRobot Romeo 2 (has built in motor controller):
+   http://www.dfrobot.com/wiki/index.php/Romeo_V2-All_in_one_Controller_(R3)_(SKU:DFR0225)
+
+   mount panel for board:
+   http://www.thingiverse.com/thing:1377159
+
+   line follower array from Sparkfun:
+   https://github.com/sparkfun/Line_Follower_Array
+   https://learn.sparkfun.com/tutorials/sparkfun-line-follower-array-hookup-guide
+   position provided ranges from -127 (far L) to 127 (far R)
+
+   PID quick tutorial
+   http://letsmakerobots.com/node/39972
+
+*/
 
 
 #include "Wire.h"              // for I2C
@@ -36,25 +36,15 @@ const uint8_t SX1509_ADDRESS = 0x3E;  // SX1509 I2C address (00)
 
 SensorBar mySensorBar(SX1509_ADDRESS);
 
-// will try to avoid floating point math
-const byte Kp = 7;
-const byte Ki = 0;
-const byte Kd = 16;
+// I know we should try to avoid floating point math, but probably will need fractional values for the constants
+const float Kp = 2;
+const float Ki = 0;
+const float Kd = 0;
 
-
-const byte MAXSPEED = 255; 
+const byte MAXSPEED = 255;
 const byte RUNSPEED = 128; // half speed for init training of faster motors
-int Lspeed = RUNSPEED;     // int not byte since may exceed 255 in calculations, but will ultimately be constrained
-int Rspeed = RUNSPEED;
-
-
 
 const int ButtonPin = 0;
-int buttonVal = 0;
-boolean goFlag = false;
-int error = 0;
-int lastError = 0;
-
 
 // Romeo standard pins
 int Lmotor = 5;                // M1 Speed Control
@@ -78,71 +68,71 @@ void setup() {
   mySensorBar.clearInvertBits();
   //Other option: light line on dark
   //mySensorBar.setInvertBits();
-  
+
   //Don't forget to call .begin() to get the bar ready.  This configures HW.
   uint8_t returnStatus = mySensorBar.begin();
- /*
-  if(returnStatus)
-  {
-	  Serial.println("sx1509 IC communication OK");
-  }
-  else
-  {
-	  Serial.println("sx1509 IC communication FAILED!");
-  }
-  Serial.println();
+  /*
+    if(returnStatus)
+    {
+    Serial.println("sx1509 IC communication OK");
+    }
+    else
+    {
+    Serial.println("sx1509 IC communication FAILED!");
+    }
+    Serial.println();
   */
-  
+
 } // end setup()
+
 
 void loop() {
 
-  buttonVal = analogRead(ButtonPin);
+  static int Lspeed = RUNSPEED; // int not byte since may exceed 255 in calculations, but will ultimately be constrained
+  static int Rspeed = RUNSPEED;
+  static boolean goFlag = false;
   
-  if (buttonVal < 30){       // button 1 - use to pause if have to stop robot
+  int buttonVal = analogRead(ButtonPin);
+
+  if (buttonVal < 30) {      // button 1 - use to pause if have to stop robot
     halt();
     goFlag = false;
     mySensorBar.setBarStrobe(); // Default: IR will only turn on during reads - saves battery
   }
-  else if (buttonVal < 175){ // button 2 
-//    //for future use
+  else if (buttonVal < 175) { // button 2
+    //    //for future use
   }
-//  else if (buttonVal < 360){  // button 3 
-//    // for future use
-//  }
-//  else if (buttonVal < 540){  // button 4
-//    // for future use
-//  }
+  //  else if (buttonVal < 360){  // button 3
+  //    // for future use
+  //  }
+  //  else if (buttonVal < 540){  // button 4
+  //    // for future use
+  //  }
 
-  else if (buttonVal < 800){  // button 5 - run line follower program
+  else if (buttonVal < 800) { // button 5 - run line follower program
     goFlag = true;
     mySensorBar.clearBarStrobe(); // run sensor all the time
     delay(3000); //  3 sec delay to back off
     // include visual indicator later
   }
+
+  static int I = 0;
+  static int lastP = 0;
   
   if (goFlag) {
-    error = mySensorBar.getPosition(); // position gives distance from midline, i.e. the error
+    int P = mySensorBar.getPosition(); // position gives distance from midline, i.e. the error
+    I = (I + P);
+    int D = (P - lastP);
+    lastP = P;
 
-    // want to be as fast as possible, so will just slow down necessary wheel for correction
-    // rather than trying to speed up one and slow the other
-    if (error < 0){           // robot has drifted right; slow down L wheel
-      Rspeed = MAXSPEED;
-      Lspeed = MAXSPEED + (Kp * error) + (Kd * (error - lastError)); // plus since error is negative, will result in negative values for proportionate term
-      Lspeed = constrain(Lspeed, 0, MAXSPEED);
-      
-    }
-    else if (error > 0){      // robot has drifted L; slow down R wheel         
-      Rspeed = MAXSPEED - (Kp * error) - (Kd * (error - lastError)); 
-      Rspeed = constrain(Rspeed, 0, MAXSPEED);
-      Lspeed = MAXSPEED;
-    }
-    else{                   // position is zero; full on both
-      Rspeed = MAXSPEED;
-      Lspeed = MAXSPEED;
-    }
+    correction = (P * Kp) + (I * Ki) + (D * Kd)
 
-    fwd(Lspeed,Rspeed);
+    // since not running full speed can speed up on side of error and slow down other side.
+    Lspeed = RUNSPEED - correction
+    Rspeed = RUNSPEED + correction
+    Lspeed = constrain(Lspeed, 0, MAXSPEED);
+    Rspeed = constrain(Rspeed, 0, MAXSPEED);
+    fwd(Lspeed, Rspeed);
 
   } // end if (goFlag)
 } // end loop()
@@ -150,38 +140,38 @@ void loop() {
 
 void halt(void)               // Stop
 {
-  digitalWrite(Lmotor,LOW);   
-  digitalWrite(Rmotor,LOW);      
-}   
+  digitalWrite(Lmotor, LOW);
+  digitalWrite(Rmotor, LOW);
+}
 
 
-void fwd(byte l,byte r)       // Move forward
+void fwd(byte l, byte r)      // Move forward
 {
-  analogWrite (Lmotor,l);     // PWM Speed Control
-  digitalWrite(Ldir,LFWD);     
-  analogWrite (Rmotor,r);    
-  digitalWrite(Rdir,RFWD);     
-}  
+  analogWrite (Lmotor, l);    // PWM Speed Control
+  digitalWrite(Ldir, LFWD);
+  analogWrite (Rmotor, r);
+  digitalWrite(Rdir, RFWD);
+}
 
 // don't need these functions for basic line follower
 //void rev(byte l,byte r)       // Reverse
 //{
 //  analogWrite (Lmotor,l);
-//  digitalWrite(Ldir,LREV);   
-//  analogWrite (Rmotor,r);    
+//  digitalWrite(Ldir,LREV);
+//  analogWrite (Rmotor,r);
 //  digitalWrite(Rdir,RREV);
-//}  
+//}
 //void spinR(byte l, byte r)
 //{
 //  analogWrite (Lmotor,l);
 //  digitalWrite(Ldir,LFWD);    // L fwd, R rev to spin R (clockwise)
-//  analogWrite (Rmotor,r);    
+//  analogWrite (Rmotor,r);
 //  digitalWrite(Rdir,RREV);
-//}  
+//}
 //void spinL(byte l, byte r)
 //{
 //  analogWrite (Lmotor,l);
 //  digitalWrite(Ldir,LREV);    // R fwd, L rev to spin L (counterclockwise)
-//  analogWrite (Rmotor,r);    
+//  analogWrite (Rmotor,r);
 //  digitalWrite(Rdir,RFWD);
-//}  
+//}
